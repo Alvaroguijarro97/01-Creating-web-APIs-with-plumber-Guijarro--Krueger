4d090209b17c9aae2badce8d29c2b0d6f5f778f7
Alvaroguijarro97 <alvaroguijarro97@gmail.com>
2022-11-16 18:42:19 +0100

    Finished Workshop

--- Workshop API.R
+++ Workshop API.R
@@ -3,7 +3,6 @@ library(ggplot2) #Library to plot
 library(dplyr) #Library for Data Wrangling
 inventory <- read.csv("inventory.csv", stringsAsFactors = FALSE)
 
-
 # Create Title of API -----------------------------------------------------
 
 #In this section we will use @apiTitle to add the Title of our API and @apiDescription to add a small description. 
@@ -11,63 +10,75 @@ inventory <- read.csv("inventory.csv", stringsAsFactors = FALSE)
 #           -"#*" is used for Plumber Comments
 #           -"@" is used to define Plumber characteristics 
 
-#* @apiTitle Excercise: Auto Inventory Manager
+#* @apiTitle Excercise: Used American Automobiles Inventory Manager
 #* @apiDescription Discover the different applications of an API while managing the inventory of an automobile store.
 
-
+#* @apiTag General Functionality having to do with the management of the API.
+#* @apiTag Cars Functionality having to do with the understanding of different variables within the car inventory.
+#* @apiTag Modify Functionality having to do with the management of car inventory.
 
 # Check if the API is running correctly! ----------------------------------
-
 #* API status check
 #* This will check if the API is up and running correctly on our machine. 
 #* @get /status-check
 #* @tag General
 status <- function(){
-  list(
-    status = "Working Perfectly",
-    time = Sys.time()
-  )
+
+  
 }
+
+# Now that we know that the API is running correctly, let's set up some requests 
+# A quick reminder of the different commands in the plumber pipeline:
+# - @get : request a resource
+# - @post : send data in body
+# - @put : store/update data
+# - @delete : delete resource
+# - @head : no request 
+# - @option : describe options
+# - @patch : partial changes
+# - @use : use all methods
 
 # Create your first Plumber Pipeline --------------------------------------
-
-#* List all cars in the inventory.
+#* List all cars in the inventory
 #* This will provide the user with all the data points in the inventory. 
-#* @get /car/ 
+#* @get /car/
 #* @tag Cars
 listCars <- function(){
-  inventory
+
 }
 
-#*Find the cars in inventory by manufacturer.
-#* Lookup cars by manufacturer (Options: Buick, Chevrolet, Ford, GMC, Nissan, Toyota, Volvo)
-#* @param make The name of the manufacturer of the car 
-#* @get /car/<make:character>
-#* @response 404 No car with the given maker was found in the inventory.
+#* Look up a car by ID
+#* This will provide the user with all the information regarding a car. 
+#* @param id The ID of the car to get
+# ^ what parameter would you like to look for in your request
+#* @get /car/<id:int>
+#* @response 404 No car with the given ID was found in the inventory.
 #* @tag Cars
-getCar <- function(make, res){
-  car <- inventory[inventory$make == make,]
+getCar <- function(id, res){
+  car <- inventory[inventory$id == id,]
   if (nrow(car) == 0){
     res$status <- 404
   }
   car
 }
 
-
-#* Create a Scatter Plot 
-#* See the relationship between mileage and year a car was manufactured.
-#* @serializer png
-#* @get /plot_scatter
-#* @tag Cars
-getChart2 <-  function() {
-  c <- ggplot(inventory, aes(x=year, y=miles, color=make)) + 
-    geom_point(size=6) +
-    scale_y_continuous(name="Car Mileage", breaks= waiver(),limits=c(15000, 300000))+
-    scale_x_continuous(name="Year", limits=c(1990, 2020))
-  print(c)
+validateCar <- function(make, model, year){
+  if (missing(make) || nchar(make) == 0){
+    return("No make specified")
+  }
+  if (missing(model) || nchar(model) == 0){
+    return("No make specified")
+  }
+  if (missing(year) || as.integer(year) == 0){
+    return("No year specified")
+  }
+  NULL
 }
 
-#* Create a Frequency chart 
+
+# Let's Plot some information! --------------------------------------------
+
+#* Count cars in inventory by manufacturer.
 #* See how many cars are in the inventory from the different makers.
 #* @serializer png
 #* @get /plot_frequency
@@ -81,6 +92,20 @@ getChart1 <- function() {
   print(b)
 }
 
+#* Observe cars by mileage and manufacturing year.
+#* See the relationship between mileage and year a car was manufactured.
+#* @serializer png
+# ^In what format do we want to get our request?  @serializer (Options: JSON, png, csv, ...etc)
+#* @get /plot_scatter
+#* @tag Cars
+getChart2 <-  function() {
+  c <- ggplot(inventory, aes(x=year, y=miles, color=make)) + 
+    geom_point(size=6) +
+    scale_y_continuous(name="Car Mileage", breaks= waiver(),limits=c(0, 300000))+
+    scale_x_continuous(name="Year", limits=c(1990, 2020))+
+    labs(title = "Distribution of cars by mileage and manufacturing year", color = "Manufacturer")
+  print(c)
+}
 
 #* Average price per year
 #* See what is the average selling price for cars by the year they were manufactured.
@@ -102,12 +127,26 @@ getChart3 <-  function() {
   print(d)
 }
 
+#Try it for yourself! Create a bar chart to see for each car manufacturer the sum of the prices for the cars in stock, separated by year.
+
+#* Total worth of the inventory by year and manufacturer
+#* @serializer png
+#* @get /plot_inventory_price
+#* @tag Cars
+getChart4 <-  function() {
+
+  
+  
+  
+}
 
 
+
+# Modify some information on the data set! --------------------------------
 
 #* Add a car to the inventory
 #* @post /car/
-#* @param make:character The make of the car
+#* @param make:character The manufacturer of the car
 #* @param model:character The model of the car
 #* @param edition:character Edition of the car
 #* @param year:int Year the car was made
@@ -117,13 +156,13 @@ getChart3 <-  function() {
 #* @tag Modify
 addCar <- function(make, model, edition, year, miles, price, res){
   newId <- max(inventory$id) + 1
-
+  
   valid <- validateCar(make, model, year)
   if (!is.null(valid)){
     res$status <- 400
     return(list(errors=paste0("Invalid car: ", valid)))
   }
-
+  
   car <- list(
     id = newId,
     make = make,
@@ -133,7 +172,7 @@ addCar <- function(make, model, edition, year, miles, price, res){
     miles = miles,
     price = price
   )
-
+  
   inventory <<- rbind(inventory, car)
   getCar(newId)
 }
@@ -149,13 +188,13 @@ addCar <- function(make, model, edition, year, miles, price, res){
 #* @put /car/<id:int>
 #* @tag Modify
 updateCar <- function(id, make, model, edition, year, miles, price, res){
-
+  
   valid <- validateCar(make, model, year)
   if (!is.null(valid)){
     res$status <- 400
     return(list(errors=paste0("Invalid car: ", valid)))
   }
-
+  
   updated <- list(
     id = id,
     make = make,
@@ -165,11 +204,11 @@ updateCar <- function(id, make, model, edition, year, miles, price, res){
     miles = miles,
     price = price
   )
-
+  
   if (!(id %in% inventory$id)){
     stop("No such ID: ", id)
   }
-
+  
   inventory[inventory$id == id, ] <<- updated
   getCar(id)
 }
